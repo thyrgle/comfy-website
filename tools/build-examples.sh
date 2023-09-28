abb802abf6c7262f8c7468919a43144671c07435
Jakub Arnold <darthdeus@gmail.com>
2023-09-28 23:53:01 +0200

    Configurable example build

--- tools/build-examples.sh
+++ tools/build-examples.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 set -euxo pipefail
 
-mkdir -p tmp
-if [ ! -d "tmp/comfy" ]; then
-  git clone --depth=1 git@github.com:darthdeus/comfy.git tmp/comfy
-else
-  echo "Directory tmp/comfy already exists, skipping clone."
-fi
-
 parent_dir="$PWD"
 
-cd tmp/comfy
+local_build=true
 
-git pull origin master
+if [ "$local_build" = true ]; then
+  cd ../comfy
+else
+  mkdir -p tmp
+  if [ ! -d "tmp/comfy" ]; then
+    git clone --depth=1 git@github.com:darthdeus/comfy.git tmp/comfy
+  else
+    echo "Directory tmp/comfy already exists, skipping clone."
+  fi
+
+  cd tmp/comfy
+  git pull origin master
+fi
 
 mkdir -p target/generated/
 mkdir -p target/videos/
